45b179a45626ae7e6e6b85cc05db678618540371
lhseokool <lhseokool@gmail.com>
2023-11-26 03:35:26 +0900

    minor change

--- 02_rag.py
+++ 02_rag.py
@@ -75,58 +75,60 @@ def get_conversation_chain(vetorestore, openai_api_key):
     return conversation_chain
 
 
-st.set_page_config(page_title="HistoryChat", page_icon=":books:")
-st.title("_History :red[QA Chat]_ :books:")
+def main():
+    st.set_page_config(page_title="HistoryChat", page_icon=":books:")
+    st.title("_History :red[QA Chat]_ :books:")
 
-if "conversation" not in st.session_state:
-    st.session_state.conversation = None
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = None
-if "processComplete" not in st.session_state:
-    st.session_state.processComplete = None
-    
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = [{"role": "assistant", 
-                                     "content": "역사에 관해 궁금하신 것이 있으면 언제든 물어봐주세요!"}]
-
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if 'vectorstore' not in st.session_state:
-    st.session_state.vectorstore = None
-    doc_list = load_and_process_data(data_dir)
-    text_chunks = get_text_chunks(doc_list)
-    vetorestore = get_vectorstore(text_chunks)
-
-    st.session_state.conversation = get_conversation_chain(vetorestore, openai_api_key) 
-    st.session_state.processComplete = True
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+    if "processComplete" not in st.session_state:
+        st.session_state.processComplete = None
+        
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = [{"role": "assistant", 
+                                        "content": "역사에 관해 궁금하신 것이 있으면 언제든 물어봐주세요!"}]
 
 
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
+    if 'vectorstore' not in st.session_state:
+        st.session_state.vectorstore = None
+        doc_list = load_and_process_data(data_dir)
+        text_chunks = get_text_chunks(doc_list)
+        vetorestore = get_vectorstore(text_chunks)
 
+        st.session_state.conversation = get_conversation_chain(vetorestore, openai_api_key) 
+        st.session_state.processComplete = True
 
-history = StreamlitChatMessageHistory(key="chat_messages")
+    history = StreamlitChatMessageHistory(key="chat_messages")
 
-# Chat logic
-if query := st.chat_input("질문을 입력해주세요."):
-    st.session_state.messages.append({"role": "user", "content": query})
+    # Chat logic
+    if query := st.chat_input("질문을 입력해주세요."):
+        st.session_state.messages.append({"role": "user", "content": query})
 
-    with st.chat_message("user"):
-        st.markdown(query)
+        with st.chat_message("user"):
+            st.markdown(query)
 
-    with st.chat_message("assistant"):
-        chain = st.session_state.conversation
+        with st.chat_message("assistant"):
+            chain = st.session_state.conversation
 
-        with st.spinner("Thinking..."):
-            result = chain({"question": query})
-            with get_openai_callback() as cb:
-                st.session_state.chat_history = result['chat_history']
+            with st.spinner("Thinking..."):
+                result = chain({"question": query})
+                with get_openai_callback() as cb:
+                    st.session_state.chat_history = result['chat_history']
+                    
+                response = result['answer']
+                st.markdown(response)
+
                 
-            response = result['answer']
-            st.markdown(response)
+        # Add assistant message to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
-            
-    # Add assistant message to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+
+# 메인 함수 호출
+if __name__ == "__main__":
+    main()
